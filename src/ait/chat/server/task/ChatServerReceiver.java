6c3b05f18ebb33373a1190cce2fbd2d90036c004
edward <edwardlfish@gmail.com>
2025-06-17 09:41:28 +0300

    Complete ChatServerReceiver

--- src/ait/chat/server/task/ChatServerReceiver.java
+++ src/ait/chat/server/task/ChatServerReceiver.java
@@ -1,5 +1,8 @@
 package ait.chat.server.task;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 
@@ -14,6 +17,21 @@ public class ChatServerReceiver implements Runnable{
 
     @Override
     public void run() {
-        // TODO Homework
+        try (Socket socket = this.socket) {
+            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (true) {
+                String message = socketReader.readLine();
+                if (message == null) {
+//                    System.out.println("Connection: " + socket.getInetAddress() + ":" + socket.getPort() + ", closed");
+//                    break;
+                    throw new IOException();
+                }
+                messageBox.put(message);
+            }
+        } catch (IOException e) {
+            System.out.println("Connection: " + socket.getInetAddress() + ":" + socket.getPort() + ", closed");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
